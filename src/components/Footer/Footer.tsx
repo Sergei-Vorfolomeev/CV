a6340dc019a319a1cdc8e27141b852ae1fe511b0
Sergei Vorfolomeev <vorfo1897@gmail.com>
2022-12-13 20:09:55 +0100

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './Footer.module.css'
 
 export const Footer = () => {
     return (
-        <div>
-
+        <div className={styles.footerPage}>
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <h2>Sergey Vorfolomeev</h2>
+                </div>
+                <div className={styles.icons}>
+                    <div className={styles.iconItem}></div>
+                    <div className={styles.iconItem}></div>
+                    <div className={styles.iconItem}></div>
+                    <div className={styles.iconItem}></div>
+                </div>
+                <span>© 2022. All rights reserved</span>
+            </div>
         </div>
     );
 };
